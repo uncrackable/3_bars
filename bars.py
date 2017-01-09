ecf3ef435265e2c0e720a0ff6df64bec99653c4a
Andrey <ibleminov@yandex.ru>
2017-01-09 14:04:07 +0300

    add load_data(), get_biggest_bar(), get_smallest_bar()

--- bars.py
+++ bars.py
@@ -1,21 +1,27 @@
 import json
-
+import math
 
 def load_data(filepath):
-    pass
+    with open(filepath,'r',encoding='cp1251') as json_to_parse:
+        json_to_parse = json.loads(json_to_parse.read())
+    return json_to_parse
 
 
 def get_biggest_bar(data):
-    pass
+    biggest_bar = max(data, key=lambda x: x['SeatsCount'])
+    return biggest_bar
 
 
 def get_smallest_bar(data):
-    pass
-
+    smallest_bar = min(data, key=lambda x: x['SeatsCount'])
+    return smallest_bar
 
 def get_closest_bar(data, longitude, latitude):
     pass
 
 
 if __name__ == '__main__':
-    pass
+    filepath = input('Enter file path: ')
+    json_content = load_data(filepath)
+    print ('Biggest bar is ', get_biggest_bar(json_content)['Name'], ':', get_biggest_bar(json_content)['SeatsCount'])
+    print ('Smallest bar is ', get_smallest_bar(json_content)['Name'], ':', get_smallest_bar(json_content)['SeatsCount'])
