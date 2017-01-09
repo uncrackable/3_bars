d900085fcd8349b44bbc2f15d9eab9b3fd2d30c5
Andrey <ibleminov@yandex.ru>
2017-01-09 14:45:41 +0300

    add get_closest_bar()

--- bars.py
+++ bars.py
@@ -17,11 +17,15 @@ def get_smallest_bar(data):
     return smallest_bar
 
 def get_closest_bar(data, longitude, latitude):
-    pass
-
+    closest_bar = min(data, key=lambda x: math.sqrt
+    ((x['geoData']['coordinates'][0] - longitude) ** 2 + (x['geoData']['coordinates'][1] - latitude) ** 2 ))
+    return closest_bar
 
 if __name__ == '__main__':
     filepath = input('Enter file path: ')
     json_content = load_data(filepath)
+    longitude = float(input('Enter Longitude: '))
+    latitude = float(input('Enter Latitude: '))
     print ('Biggest bar is ', get_biggest_bar(json_content)['Name'], ':', get_biggest_bar(json_content)['SeatsCount'])
     print ('Smallest bar is ', get_smallest_bar(json_content)['Name'], ':', get_smallest_bar(json_content)['SeatsCount'])
+    print ('Closest bar is ', get_closest_bar(json_content, longitude, latitude)['Name'])
